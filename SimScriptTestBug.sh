889409132fb871aeeaf3d0ad8ed08f4ec3f60881
Noostalgied <45884070+Noostalgied@users.noreply.github.com>
2019-04-17 18:03:43 +0200

    Update SimScriptTestBug.sh

--- SimScriptTestBug.sh
+++ SimScriptTestBug.sh
@@ -19,3 +19,4 @@ curl -i -X GET  http://localhost:8080/bug/search/findByUsuari?usuari=Marcela
 curl -i -X GET  http://localhost:8080/bug/search/deleteByUsuari?usuari=Marcela
 #Test Borrar por videojuego
 curl -i -X DELETE  http://localhost:8080/bug/RocketLeague
+
