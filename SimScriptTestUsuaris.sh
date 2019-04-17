d799b61c08ccfc14cae4be3e523dfc4030519786
Noostalgied <45884070+Noostalgied@users.noreply.github.com>
2019-04-17 18:08:17 +0200

    Update SimScriptTestUsuaris.sh

--- SimScriptTestUsuaris.sh
+++ SimScriptTestUsuaris.sh
@@ -2,7 +2,7 @@
 curl -i -X POST -H "Content-Type:application/json" -d '{"codUsuari": "mcastrol", "nom": "Marcela","cognoms":"Castro León","email":"mcastrol@gmail.com"}' http://localhost:8080/usuari
 curl -i -X POST -H "Content-Type:application/json" -d '{"codUsuari": "nico", "nom": "nico","cognoms":"Hernández","email":"nico"}' http://localhost:8080/usuari
 curl -i -X POST -H "Content-Type:application/json" -d '{"codUsuari": "kyla", "nom": "kyla","cognoms":"Hernández","dataNaixement":"2017-10-10"}' http://localhost:8080/usuari
-#Test modificacióN
+#Test modificación
 curl -i -X PUT -H "Content-Type:application/json" -d '{"codUsuari": "mcastrol", "nom": "Marcela","cognoms":"Castro León","email":"mcastrol@gmail.com","adreça":"Sabadell" }' http://localhost:8080/usuari/1
 #Test Obtener todos los datos
 curl -i -X GET http://localhost:8080/usuari
@@ -18,4 +18,5 @@ curl -i -X GET  http://localhost:8080/usuari/search/findByEmail?email=mcastrol@g
 curl -i -X GET  http://localhost:8080/usuari/search/deleteByEmail?email=mcastrol@gmail.com
 #Test Borrar por id
 curl -i -X DELETE  http://localhost:8080/usuari/2
+
 
